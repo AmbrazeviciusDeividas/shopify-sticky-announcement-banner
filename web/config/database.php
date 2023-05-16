ed7a117864af4245f0669eccd1cb7e8f313becd0
AmbrazeviciusDeividas <ambrazevicius.deividas@gmail.com>
2023-05-16 18:07:24 +0300

    add database configurations

--- web/config/database.php
+++ web/config/database.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Str;
 
+$url = parse_url(getenv("DATABASE_URL"));
+
+$host = $url["host"] ?? null;
+$username = $url["user"] ?? null;
+$password = $url["pass"] ?? null;
+$database = ltrim($url["path"], "/");
+
 return [
 
     /*
